55889c94e664a49306606d1adc9358a6660eb728
Olabanji Aladejana <olabanjialadejana@gmail.com>
2023-01-10 12:46:41 +0100

    an easier function for hihest bidder

--- Codes/Dictionaries and Nesting/secret_auction_program/main.py
+++ Codes/Dictionaries and Nesting/secret_auction_program/main.py
@@ -5,6 +5,20 @@ import art
 def add_to_bid(bidder_name, bid_price):
     bid[bidder_name] = bid_price
 
+# This is a function to get the highest bidder from {bid}
+
+
+def find_highest_bidder(bidding_record):
+    # bid i.e bidding_record = {'olabanji': 345, 'kemio': 456, 'tunji': 457}
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
 
 bid = {}
 keep_on = True
@@ -18,16 +32,17 @@ while keep_on:
     if question == 'no':
         keep_on = False
 
-all_bids = []
-for num in bid.values():
-    all_bids.append(num)
+find_highest_bidder(bid)
+# all_bids = []
+# for num in bid.values():
+#     all_bids.append(num)
 
-highest_bid = max(all_bids)
+# highest_bid = max(all_bids)
 
 
-# Get the key corresponding to value highest_bid (** I copied the code. But it somehow worked!! I have to learn how it was done!!!)
-highest_key = list(filter(lambda x: bid[x] == highest_bid, bid))[0]
+# # Get the key corresponding to value highest_bid (** I copied the code. But it somehow worked!! I have to learn how it was done!!!)
+# highest_key = list(filter(lambda x: bid[x] == highest_bid, bid))[0]
 
-# This code is contributed by Edula Vinay Kumar Reddy (i.e. the copied lambda)
+# # This code is contributed by Edula Vinay Kumar Reddy (i.e. the copied lambda)
 
-print(f'The winner is {highest_key} with a bid of ${highest_bid}')
+# print(f'The winner is {highest_key} with a bid of ${highest_bid}')
